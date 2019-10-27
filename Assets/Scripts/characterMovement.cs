65295f1c54a81a19725f053ccea95ed4af3ac53b
Hawley <nah1004@sru.edu>
2019-10-27 15:11:39 -0400

    Comments for characterMovement
    Commented characterMovement more clearly

--- Assets/Scripts/characterMovement.cs
+++ Assets/Scripts/characterMovement.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class characterMovement : MonoBehaviour
 {
+    // reference to the scenes camera
     public GameObject cam;
+    // reference to the camera container
     public GameObject camHolder;
+    //players movement speed
     public float speed;
     [Range(0,10)]
+    // players jump force
     public float jumpForce;
+    // quaternion for camera rotation
     float camRotX;
     float camRotY;
+    // look sensitivity
     [Range(.1f,9.99f)]
     public float lookSensitivity;
 
     // Start is called before the first frame update
     void Start()
     {
+        //hide cursor and lock the curson in the center
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
@@ -25,13 +32,16 @@ public class characterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if press escape unlock cursor and make it visible
         if (Input.GetKey(KeyCode.Escape)) {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
 
+        //press w
         if (Input.GetKey(KeyCode.W))
         {
+            //move the character forward relative to the camera container by speed per unit time
             gameObject.transform.Translate(camHolder.transform.forward * speed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.S)) {
